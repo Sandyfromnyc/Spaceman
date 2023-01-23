25a1359de1d5f524a15d8041c29c076381d6d760
sandyfromnyc <sandyfromnyc@gmail.com>
2023-01-23 18:01:13 -0500

    added grid and some js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
   /*----- constants -----*/
 const MAN_LOOKUP = {
-    
+
 }
 
   /*----- state variables -----*/
+let guessed;  // guessed letters to guess word
 
+let wrong; // guessed letters are wrong
 
   /*----- cached elements  -----*/
 
