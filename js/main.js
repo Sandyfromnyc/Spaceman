ce17d66dbbba79b72a9c7f54ac462e3c57e83124
sandyfromnyc <sandyfromnyc@gmail.com>
2023-01-23 19:15:41 -0500

    added constants

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
   /*----- constants -----*/
 const MAN_LOOKUP = {
-
-}
+    all: {img:'imgs/spaceman-0.jpg'},
+    one: {img:'imgs/spaceman-1.jpg'}
+    two: {img:'imgs/spaceman-2.jpg'},
+    three: {img:'imgs/spaceman-3.jpg'},
+    four: {img:'imgs/spaceman-4.jpg'},
+    five: {img:'imgs/spaceman-5.jpg'},
+    six: {img:'imgs/spaceman-6.jpg'}
+};
+const WORDS = ["PLANET", "ORBIT", 'STARS', "METEOR", "COSMET", "GALAXY", "STARBUUST", "NEBULA"];
 
   /*----- state variables -----*/
 let guessed;  // guessed letters to guess word
 
-let wrong; // guessed letters are wrong
+let wrong; // guessed letters are wrong - a piece of spaceman disappears
 
   /*----- cached elements  -----*/
 
